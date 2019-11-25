f7dc4320e054edd14b4b1eb8fdf9018d57ea79b3
vinaykotla <36473269+vinaykotla@users.noreply.github.com>
2019-11-25 16:39:02 +0530

    Update release.sh

--- release.sh
+++ release.sh
@@ -7,5 +7,7 @@ tag="latest"
 
 bash security_check.sh 
 #sudo docker build -t bizmet:latest .
-sudo docker build -t ${name}/${tag} .
+docker build -t ${name}/${tag} .
+docker stop ${name} || echo "stopped"
+docker rm ${name} || echo "deleted"
 docker run -d -p ${host_port}:${docker_port} --name=${name} ${name}/${tag}

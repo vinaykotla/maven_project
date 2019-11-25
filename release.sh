e516aad0ab40a7c08cf2172b3da63e703f9ec0f9
vinaykotla <36473269+vinaykotla@users.noreply.github.com>
2019-11-25 16:27:25 +0530

    Update release.sh

--- release.sh
+++ release.sh
@@ -8,5 +8,4 @@ tag="latest"
 bash security_check.sh 
 #sudo docker build -t bizmet:latest .
 sudo docker build -t ${name}/${tag} .
-docker run -d -p ${host_port}:${docker_port} --name=${name} ${name}
-
+docker run -d -p ${host_port}:${docker_port} --name=${name} ${name}/${tag}
